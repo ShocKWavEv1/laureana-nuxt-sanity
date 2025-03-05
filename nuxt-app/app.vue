9ab1e332eb8050a1e7fabd4132d015717bbebb48
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-03-05 14:22:48 -0600

    move to composable

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,27 +1,6 @@
 <script setup>
-import { useNuxtApp } from "#app";
-import { onMounted, onBeforeUnmount } from "vue";
-
 onMounted(() => {
-  if (process.client) {
-    const { $lenis } = useNuxtApp();
-
-    if ($lenis) {
-      // Start animation loop
-      function raf(time) {
-        $lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-      requestAnimationFrame(raf);
-    }
-  }
-});
-
-onBeforeUnmount(() => {
-  if (process.client) {
-    const { $lenis } = useNuxtApp();
-    if ($lenis) $lenis.destroy();
-  }
+  useLenisSmoothScroll();
 });
 </script>
 
