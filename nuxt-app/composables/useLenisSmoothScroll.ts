5a49d8111bc407f3dee445c27105f8c2710965a4
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-03-05 14:57:45 -0600

    duration

--- nuxt-app/composables/useLenisSmoothScroll.ts
+++ nuxt-app/composables/useLenisSmoothScroll.ts
@@ -2,7 +2,7 @@ import Lenis from "@studio-freight/lenis";
 
 export const useLenisSmoothScroll = () => {
   const lenis = new Lenis({
-    duration: 1.75,
+    duration: 2.75,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     orientation: "vertical",
     gestureOrientation: "vertical",
