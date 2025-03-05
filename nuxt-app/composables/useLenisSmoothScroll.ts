2575f735ba2137f2ac237e19eabc6eca07edbef9
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-03-05 14:25:56 -0600

    no env condition for init lenis

--- nuxt-app/composables/useLenisSmoothScroll.ts
+++ nuxt-app/composables/useLenisSmoothScroll.ts
@@ -1,21 +1,19 @@
 import Lenis from "@studio-freight/lenis";
 
 export const useLenisSmoothScroll = () => {
-  if (import.meta.client) {
-    const lenis = new Lenis({
-      duration: 1.75,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-    });
+  const lenis = new Lenis({
+    duration: 1.75,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: "vertical",
+    gestureOrientation: "vertical",
+    smoothWheel: true,
+  });
 
-    // Request animation frame for smooth scrolling
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
+  // Request animation frame for smooth scrolling
+  function raf(time: number) {
+    lenis.raf(time);
     requestAnimationFrame(raf);
   }
+
+  requestAnimationFrame(raf);
 };
