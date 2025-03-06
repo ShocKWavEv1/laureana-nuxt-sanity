32ffcab14dd867f120b529e4ca1f13ce6cafd362
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-03-05 18:12:19 -0600

    update nuxt confog

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Laureana App",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "My amazing site.",
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+    },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/sanity"],
