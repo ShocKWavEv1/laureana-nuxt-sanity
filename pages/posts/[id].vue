b2fd6cb8c640d15b3f1037ca1bec5fd799406ff6
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-03-05 18:50:15 -0600

    Nuxt App with Sanity and first groq query, installation and config of Lenis Scroll

---
+++ pages/posts/[id].vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const pageTitle = computed(() => `Laureana App - ${route.params.id}`);
+
+useHead({
+  title: pageTitle,
+  meta: [{ name: "description", content: "My amazing site." }],
+  bodyAttrs: {
+    class: "test",
+  },
+});
+</script>
+
+<template>
+  <div class="w-full h-[300vh] bg-teal-500">
+    Hi Post Detail {{ route.params.id }}
+    <NuxtLink to="/posts">Posts</NuxtLink>
+  </div>
+</template>
